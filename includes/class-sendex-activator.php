505d7d00a4194864a5381d0ae1dd8d3c1a7bce03
IMS-cOsX <for7online@gmail.com>
2021-08-19 14:55:36 +0600

    wp-sms

---
+++ includes/class-sendex-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://sms.com/sendex
+ * @since      1.0.0
+ *
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ * @author     Name <email@email.com>
+ */
+class Sendex_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-sendex-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://sms.com/sendex
+ * @since      1.0.0
+ *
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sendex
+ * @subpackage Sendex/includes
+ * @author     Name <email@email.com>
+ */
+class Sendex_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
